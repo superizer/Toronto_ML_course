1a4bdf7415b4cf22599a7b9574618413e9efe56b
superizer <superizer.tone@gmail.com>
2018-01-24 10:00:21 +0700

    finished assignment3

--- Assignment3/a3.m
+++ Assignment3/a3.m
@@ -20,7 +20,7 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
   end
   for optimization_iteration_i = 1:n_iters,
     model = theta_to_model(theta);
-    
+
     training_batch_start = mod((optimization_iteration_i-1) * mini_batch_size, n_training_cases)+1;
     training_batch.inputs = datas.training.inputs(:, training_batch_start : training_batch_start + mini_batch_size - 1);
     training_batch.targets = datas.training.targets(:, training_batch_start : training_batch_start + mini_batch_size - 1);
@@ -111,26 +111,30 @@ end
 function ret = loss(model, data, wd_coefficient)
   % model.input_to_hid is a matrix of size <number of hidden units> by <number of inputs i.e. 256>. It contains the weights from the input units to the hidden units.
   % model.hid_to_class is a matrix of size <number of classes i.e. 10> by <number of hidden units>. It contains the weights from the hidden units to the softmax units.
-  % data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>. Each column describes a different data case. 
+  % data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>. Each column describes a different data case.
   % data.targets is a matrix of size <number of classes i.e. 10> by <number of data cases>. Each column describes a different data case. It contains a one-of-N encoding of the class, i.e. one element in every column is 1 and the others are 0.
-	 
+
   % Before we can calculate the loss, we need to calculate a variety of intermediate values, like the state of the hidden units.
   hid_input = model.input_to_hid * data.inputs; % input to the hidden units, i.e. before the logistic. size: <number of hidden units> by <number of data cases>
   hid_output = logistic(hid_input); % output of the hidden units, i.e. after the logistic. size: <number of hidden units> by <number of data cases>
   class_input = model.hid_to_class * hid_output; % input to the components of the softmax. size: <number of classes, i.e. 10> by <number of data cases>
-  
+
   % The following three lines of code implement the softmax.
   % However, it's written differently from what the lectures say.
   % In the lectures, a softmax is described using an exponential divided by a sum of exponentials.
-  % What we do here is exactly equivalent (you can check the math or just check it in practice), but this is more numerically stable. 
+  % What we do here is exactly equivalent (you can check the math or just check it in practice), but this is more numerically stable.
   % "Numerically stable" means that this way, there will never be really big numbers involved.
   % The exponential in the lectures can lead to really big numbers, which are fine in mathematical equations, but can lead to all sorts of problems in Octave.
   % Octave isn't well prepared to deal with really large numbers, like the number 10 to the power 1000. Computations with such numbers get unstable, so we avoid them.
-  class_normalizer = log_sum_exp_over_rows(class_input); % log(sum(exp of class_input)) is what we subtract to get properly normalized log class probabilities. size: <1> by <number of data cases>
-  log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]); % log of probability of each class. size: <number of classes, i.e. 10> by <number of data cases>
-  class_prob = exp(log_class_prob); % probability of each class. Each column (i.e. each case) sums to 1. size: <number of classes, i.e. 10> by <number of data cases>
-  
-  classification_loss = -mean(sum(log_class_prob .* data.targets, 1)); % select the right log class probability using that sum; then take the mean over all data cases.
+  class_normalizer = log_sum_exp_over_rows(class_input);
+  % log(sum(exp of class_input)) is what we subtract to get properly normalized log class probabilities. size: <1> by <number of data cases>
+  log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]);
+  % log of probability of each class. size: <number of classes, i.e. 10> by <number of data cases>
+  class_prob = exp(log_class_prob);
+   % probability of each class. Each column (i.e. each case) sums to 1. size: <number of classes, i.e. 10> by <number of data cases>
+
+  classification_loss = -mean(sum(log_class_prob .* data.targets, 1));
+  % select the right log class probability using that sum; then take the mean over all data cases.
   wd_loss = sum(model_to_theta(model).^2)/2*wd_coefficient; % weight decay loss. very straightforward: E = 1/2 * wd_coeffecient * theta^2
   ret = classification_loss + wd_loss;
 end
@@ -138,14 +142,29 @@ end
 function ret = d_loss_by_d_model(model, data, wd_coefficient)
   % model.input_to_hid is a matrix of size <number of hidden units> by <number of inputs i.e. 256>
   % model.hid_to_class is a matrix of size <number of classes i.e. 10> by <number of hidden units>
-  % data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>. Each column describes a different data case. 
+  % data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>. Each column describes a different data case.
   % data.targets is a matrix of size <number of classes i.e. 10> by <number of data cases>. Each column describes a different data case. It contains a one-of-N encoding of the class, i.e. one element in every column is 1 and the others are 0.
 
   % The returned object is supposed to be exactly like parameter <model>, i.e. it has fields ret.input_to_hid and ret.hid_to_class. However, the contents of those matrices are gradients (d loss by d model parameter), instead of model parameters.
-	 
+
   % This is the only function that you're expected to change. Right now, it just returns a lot of zeros, which is obviously not the correct output. Your job is to replace that by a correct computation.
-  ret.input_to_hid = model.input_to_hid * 0;
-  ret.hid_to_class = model.hid_to_class * 0;
+  batchsize = size(data.inputs, 2);
+  hid_input = model.input_to_hid * data.inputs;
+  hid_output = logistic(hid_input);
+  class_input = model.hid_to_class * hid_output;
+  class_normalizer = log_sum_exp_over_rows(class_input); % log(sum(exp of class_input)) is what we subtract to get properly normalized log class probabilities. size: <1> by <number of data cases>
+  log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]); % log of probability of each class. size: <number of classes, i.e. 10> by <number of data cases>
+  class_prob = exp(log_class_prob); % probability of each class. Each column (i.e. each case) sums to 1. size: <number of classes, i.e. 10> by <number of data cases>
+  % COMPUTE DERIVATIVE.
+  error_deriv = class_prob - data.targets; % size: 10 by <number of data cases>
+  % BACK PROPAGATE.
+  hid_to_class_weight_gradient = hid_output * error_deriv'; % size: <number of hidden units> by 10
+  back_propagated_deriv = (model.hid_to_class' * error_deriv) ...
+      .* hid_output .* (1 - hid_output); % size: <number of hidden units> by <number of data cases>
+  input_to_hid_weight_gradient = data.inputs * back_propagated_deriv'; % size: 256 by <number of hidden units>
+  % REGULARIZATION
+  ret.input_to_hid = model.input_to_hid * wd_coefficient + input_to_hid_weight_gradient' ./ batchsize;
+  ret.hid_to_class = model.hid_to_class * wd_coefficient + hid_to_class_weight_gradient' ./ batchsize;
 end
 
 function ret = model_to_theta(model)
@@ -173,7 +192,7 @@ function ret = classification_performance(model, data)
   hid_input = model.input_to_hid * data.inputs; % input to the hidden units, i.e. before the logistic. size: <number of hidden units> by <number of data cases>
   hid_output = logistic(hid_input); % output of the hidden units, i.e. after the logistic. size: <number of hidden units> by <number of data cases>
   class_input = model.hid_to_class * hid_output; % input to the components of the softmax. size: <number of classes, i.e. 10> by <number of data cases>
-  
+
   [dump, choices] = max(class_input); % choices is integer: the chosen class, plus 1.
   [dump, targets] = max(data.targets); % targets is integer: the target class, plus 1.
   ret = mean(double(choices ~= targets));
